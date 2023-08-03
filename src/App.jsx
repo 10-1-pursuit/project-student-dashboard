4797c0782064046c828052c9605ff012992ec724
Jesse Chong <jessechong@pursuit.org>
2023-08-03 14:37:33 -0400

    Finished off track and on track status

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,22 @@ console.log(data)
 function App() {
   const [studentsData, setStudentsData] = useState(data);
 
+  const calculateOnTrackStatus = (student) => {
+    const hasResumeCertification = student.certifications.resume;
+    const hasLinkedInCertification = student.certifications.linkedin;
+    const hasGitHubCertification = student.certifications.github;
+    const hasMockInterviewCertification = student.certifications.mockInterview;
+    const currentCodeWarsScore = student.codewars.current.total;
+
+    return (
+      hasResumeCertification &&
+      hasLinkedInCertification &&
+      hasGitHubCertification &&
+      hasMockInterviewCertification &&
+      currentCodeWarsScore > 600
+    )
+  }
+
   return (
     <div>
       <h1>Student Dashboard</h1>
@@ -16,7 +32,8 @@ function App() {
           <h3>{student.names.preferredName}</h3>
           <p>Email: {student.username}</p>
           <p>Birthday: {student.dob}</p>
-          <img src={student.profilePhoto} alt={student.names.prefferedName} />
+          <img src={student.profilePhoto} alt={student.names.preferredName} />
+          <p>{calculateOnTrackStatus(student) ? "On Track" : "Off Track"} to Graduate</p>
         </li>
       ))}
     </ul>
