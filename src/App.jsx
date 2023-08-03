f03fb380d50a4f16cbc961f24f0e4dbca784459d
keniaDE <decoteaukenia@gmail.com>
2023-08-03 14:17:35 -0400

    added student list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
+import studentData from "./data/data.json"
+import "./index.css"
+
+
+
+
+const studentsToRender = studentData.map(
+  (eachStudentObj) => {
+    return (
+      <div className="Student">
+        <img src={eachStudentObj.profilePhoto} alt={`${eachStudentObj.names.preferredName}`} />
+        <h4>{eachStudentObj.names.preferredName}</h4>
+        <p>{eachStudentObj.username}</p>
+        <span>Birthday: {eachStudentObj.dob}</span> <br />
+
+      </div>)
+  })
+
+
+
+
+
+
+
+
+
+
 
 function App() {
+
   return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
+    <>
+   
+      <div>
+      <header>
+        <h1>Student Dashboard</h1>
+        </header>
+      </div>
+      <div>
+        <h2> All Students</h2>
+      </div>
+      {studentsToRender}
+    </>
   );
 }
 
