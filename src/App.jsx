7219b1f0b1d34bdb39629394791d82e327081d89
Natyka Callwood <Natykacallwood@pursuit.org>
2023-08-04 14:06:54 -0400

    Added a unique list of the cohorts to App.jsx. Was able to get just one of each cohort time in a list on the page on load. Still need to have the list be 'human readable' and in time order.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import data from "./data/data.json"
 //console.log(data)
 
 function App() {
+  const totalofAllStudents = data.length
 
   const studentListForHomePage = data.map(
     (eachStudentToListObj)=>{
@@ -16,10 +17,22 @@ function App() {
     }
   )
 
+  // Javasripttuturial.net way below
+ const uniqueCohortList = [...new Map(data.map((d)=>[d.cohort.cohortCode, d])).values()]
+//  console.log(uniqueCohortList) 
+  const cohortListForHomePage = uniqueCohortList.map(
+    (eachCohortNameForList)=>{
+      return(<div>
+        <h4>{eachCohortNameForList.cohort.cohortCode}</h4>
+      </div>)
+    }
+  )
 
   return (
     <div>
       <h1>Student Dashboard</h1>
+      <h3>Total Students: {totalofAllStudents}</h3>
+      {cohortListForHomePage}
       {studentListForHomePage}
     </div>
   );
