e64e4ca67c5553a8deb9a133699ccb88eabead85
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-08-03 20:07:32 -0400

    student list created, shows name_email_dob & profile picture, no css yet

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,29 @@ import Header from "./Components/Header.jsx";
 function App() {
 
   return (
-    <div className="App">
+    <div className="app">
       <>
         <Header />
       </>
+      <main>
+        <div className="student-list">
+          <h3>Student List</h3>
+          {studentData.map(student => {
+            return (
+              <div key={student.id} className="student-card">
+                <div>
+                  <img src={student.profilePhoto} alt={student.names.preferredName} />
+                </div>
+                <div>
+                  <h2>{`${student.names.preferredName} ${student.names.middleName} ${student.names.surname}`}</h2>
+                  <p>{student.username}</p>
+                  <p>{student.dob}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </main>
     </div>
   );
 }
