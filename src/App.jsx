add810fa400178efd5aa5c029ce200b3ddc47fa5
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-08-06 01:43:25 -0400

    modified Header and added data to App.jsx. My header is showing on page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,30 @@ import data from "./data/data.json"
 import Header from "./components/Header.jsx"
 import Aside from "./components/Aside.jsx"
 import Main from "./components/Main.jsx"
+import "./components/kpi.css"
+import "./components/filteredStudents.css"
+import "./components/header.css"
+import "./components/form.css"
 
 
 function App() {
+  const [studentsList, setStudentsList] = useState(data);
+  const [studentsByCode, setStudentsByCode] = useState(handleStuByCohortCode())
+
+  const [selectedCohort, setSelectedCohort] = useState("")
+  const [selectedStudentsList, setSelectedStudentsList] = useState([])
+  
+  function handleStuByCohortCode(){
+    let stuByCohort = {}
+  }
+
   return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
-  );
+  <div>
+
+
+  <Header studentsList={studentsList} /> 
+  </div>
+  )
 }
 
 export default App;
