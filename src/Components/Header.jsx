50c63d81d121b44222dc75c144e9bab9f53a5d1a
Jesse Chong <jessechong@pursuit.org>
2023-08-07 11:36:47 -0400

    Added Header.jsx and stylized basic page layout

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="student-dashboard">
+            <h1>Student Dashboard</h1>
+        </div>
+    )
+}
+
+export default Header;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,9 @@
 import StudentProfile from "./StudentProfile";
 import data from '../data/data.json';
 import { useState } from "react";
-import FormatCohortNames, { formatCohortCode } from './FormatCohortNames'
+import "./Home.css";
+import Header from './Header';
+import FormatCohortNames, { formatCohortCode } from './FormatCohortNames';
 
 const Home = () => {
     const studentsData = data;
@@ -13,21 +15,31 @@ const Home = () => {
     console.log(filteredStudents);
 
     return (
-        <div>
+      <div>
+        <div className="header">
+          <Header />
+          </div>
+          <div className="container">
+            <div className="cohort-list">
           <FormatCohortNames studentsData={studentsData} setSelectedCohort={setSelectedCohort}/>
-        <h1>Student Dashboard</h1>
+        </div>
+        <div className="student-right">
         {selectedCohort ? (
           <h2>{formatCohortCode(selectedCohort)}</h2>
         ) : (
           <h2>All Students</h2>
         )}
         <p>Total Students: {filteredStudents.length}</p>
+        <div className="student-profile">
       <ul>
         {filteredStudents.map((student) => (
           <StudentProfile key={student.id} student={student} />
         ))}
       </ul>
       </div>
+      </div>
+      </div>
+</div>
     )
 }
 
