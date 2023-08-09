86636cc1de61daa07fa2f5a9e7e27484eef9d249
yulondawyatt@gmail.com <yulondajoywyatt@Yulondas-MacBook-Air.local>
2023-08-08 22:00:40 -0400

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import data from "./data/data.json"
+
+console.log(data)
 
 function App() {
+  
+  // WILL NEED TO USE STATE
+
+  const renderStudents = data.map( 
+    
+    (eachStudentObj)=>{
+      
+      return(
+        <div>
+      <img src="" alt="Student Profile Pic" />
+      <h3>{eachStudentObj.username}</h3>
+        </div>
+        )
+      }
+    
+    )
+
   return (
+  
     <div>
+    
       <h1>Student Dashboard</h1>
+      {renderStudents}
     </div>
+    
+  
   );
 }
 
