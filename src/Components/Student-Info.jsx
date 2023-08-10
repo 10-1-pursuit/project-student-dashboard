fe5d34a774a9f9fc3a66559c9763aa5216a57a52
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-10 06:58:20 -0400

    full details are visible

--- src/Components/Student-Info.jsx
+++ src/Components/Student-Info.jsx
@@ -1,4 +1,5 @@
 import data from "../data/data.json";
+import { useState } from "react";
 
 const ViewInfo = () => {
     return (
@@ -9,70 +10,57 @@ const ViewInfo = () => {
     )
 };
 
-export default ViewInfo;
+const StudentCards = () => {
 
-function StudentCards() {
+    const [studentCohort, setStudentCohort] = useState(data);
 
-    const allStudentObj =
-        data.map((studentObj) => {
-            return {
-                profilePhoto: studentObj.profilePhoto,
-                preferredName: studentObj.names.preferredName,
-                middleName: studentObj.names.middleName,
-                surname: studentObj.names.surname,
-                username: studentObj.username,
-                dob: studentObj.dob,
-                currentTotal: studentObj.codewars.current.total,
-                lastWeek: studentObj.codewars.current.lastWeek,
-                goal: studentObj.codewars.goal.total,
-                percentOfGoalAchieved: studentObj.codewars.percentOfGoalAchieved,
-                assignments: studentObj.cohort.scores.assignments * 100,
-                projects: studentObj.cohort.scores.projects * 100,
-                assessments: studentObj.cohort.scores.assessments * 100,
-                resume: studentObj.certifications.resume,
-                linkedIn: studentObj.certifications.linkedin,
-                mockInterview: studentObj.certifications.mockInterview,
-                gitHub: studentObj.certifications.github
-            }
+    const studentstoRender =
+        studentCohort.map((studentObj) => {
+            return (
+                <>
+                    < section className="card-container" >
+                        <section className="student-top-info">
+                        <img className="student-img" src={studentObj.profilePhoto} alt="Student Profile Photo" />
+                        <div className="badge-info-container">
+                            <h3 className="badge-info"> {studentObj.names.preferredName + " " + studentObj.names.middleName.at(0) + ". " + studentObj.names.surname}</h3>
+                            <p className="badge-info">{studentObj.username}</p>
+                            <p className="badge-info"><span className="green-text">Birthday:</span> {studentObj.dob}</p>
+                            <br />
+                            <p className="badge-expander">Show More...</p>
+                        </div>
+                    </section >
+                    <section className="student-details">
+                        <div className="codewars-container">
+                            <p><span className="green-text">Current Total:</span> {studentObj.codewars.current.total}</p>
+                            <p><span className="green-text">Last Week:</span> {studentObj.codewars.current.lastWeek}</p>
+                            <p><span className="green-text">Goal:</span> {studentObj.codewars.goal.total}</p>
+                            <p><span className="green-text">Percent of Goal Achieved:</span> {studentObj.codewars.percentOfGoalAchieved}</p>
+                        </div>
+                        <div className="scores-container">
+                            <p><span className="green-text">Assignments:</span> {studentObj.cohort.scores.assignments * 100}</p>
+                            <p><span className="green-text">Projects:</span> {studentObj.cohort.scores.projects * 100}</p>
+                            <p><span className="green-text">Assessments:</span> {studentObj.cohort.scores.assessments * 100}</p>
+                        </div>
+                        <div className="certifications-container">
+                            <p><span className="green-text">Resume:</span>{studentObj.certifications.resume ? "✅" : "❌"}</p>
+                            <p><span className="green-text">LinkedIn</span>:{studentObj.certifications.linkedin ? "✅" : "❌"}</p>
+                            <p><span className="green-text">Mock Interview:</span>{studentObj.certifications.mockInterview ? "✅" : "❌"}</p>
+                            <p><span className="green-text">GitHub:</span>{studentObj.certifications.github ? "✅" : "❌"}</p>
+                        </div>
+                        </section>
+                    </section >
+                </>
+
+            )
         });
 
-    allStudentObj.map((studentObj) => {
-        return (
-            <>
-                <section className="card-container">
-                    <img className="student-img" src={studentObj.profilePhoto} alt="Student Profile Photo" />
-                    <div className="badge-info-container">
-                        <h3 className="badge-info"> {studentObj.preferredName + " " + studentObj.middleName + ". " + studentObj.surname}</h3>
-                        <p className="badge-info">{studentObj.username}</p>
-                        <p className="badge-info"><span className="green-text">Birthday:</span> {studentObj.dob}</p>
-                        <br />
-                        <p className="badge-expander">Show More...</p>
-                    </div>
-                </section>
-                {/* <section className="student-details">
-                    <div className="codewars-container">
-                        <p className="green-text">Current Total:{ }</p>
-                        <p className="green-text">Last Week:{ }</p>
-                        <p className="green-text">Goal:{ }</p>
-                        <p className="green-text">Percent of Goal Achieved:{ }</p>
-                    </div>
-                    <div className="scores-container">
-                        <p className="green-text">Assignments:{ }</p>
-                        <p className="green-text">Projects:{ }</p>
-                        <p className="green-text">Assessments:{ }</p>
-                    </div>
-                    <div className="certifications-container">
-                        <p className="green-text">Resume:{ }</p>
-                        <p className="green-text">LinkedIn:{ }</p>
-                        <p className="green-text">Mock Interview:{ }</p>
-                        <p className="green-text">GitHub:{ }</p>
 
-                    </div>
-                </section> */}
-            </>
-        )
-    }
+    return (
+        <>
+            {studentstoRender}
+        </>
     )
-}
+};
+
 
 export { StudentCards, ViewInfo };
